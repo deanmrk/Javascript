a7896b78144b9bf05abd622a6c208decfffe1756
Deanmark <ayayaji21@gmail.com>
2025-12-28 22:39:06 +0800

    day 1

---
+++ day 1/artimetic operations/index.js
@@ -0,0 +1,44 @@
+let students = 40;
+
+console.log(students);
+//Add
+students = students + 1; //re assign the students value
+console.log(students); //41
+
+//Subtract
+students = students - 1; //re assign the students value
+console.log(students); //40
+
+//Multiply
+students = students * 3; //re assign the students value
+console.log(students); //120
+
+//Division
+students = students / 2; //re assign the students value
+console.log(students); //60
+
+//Exponents
+students = students ** 2; //re assign the students value
+console.log(students); //3600
+
+//Modulus
+students = students % 2; //re assign the students value
+console.log(students); //0
+
+//augmented
+let newStudents = 10;
+
+newStudents += 1;
+console.log(newStudents); //11
+
+newStudents -= 1;
+console.log(newStudents); //10
+
+newStudents *= 2;
+console.log(newStudents); //10
+
+newStudents /= 2;
+console.log(newStudents); //10
+
+newStudents++; //increment
+newStudents--; //decrement
