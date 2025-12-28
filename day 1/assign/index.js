2c0bfc70f9c60c06bfe7f3605a42b4c7c1b78670
Deanmark <ayayaji21@gmail.com>
2025-12-28 22:10:43 +0800

    first commit

---
+++ day 1/assign/index.js
@@ -0,0 +1,16 @@
+console.log("hello world!");
+console.log("Day 1");
+
+//window.alert(`This is an alert`);
+
+//comments
+
+/* 
+multi line comment
+this is a comment
+*/
+
+document.getElementById("text1").textContent = `Day 1`;
+document.getElementById(
+  "text2"
+).textContent = `this is a day 1 in my javascript learning`;
---
+++ day 1/variable/index.js
@@ -0,0 +1,29 @@
+//THIS IS (``) a template literal
+
+let age = 20;
+let price = 10.55;
+let total;
+
+console.log(age);
+console.log(price);
+total = age + price;
+
+console.log(`The total is ${total}`);
+
+let userName = "Dean Mark";
+let address = "Pangasinan";
+
+//STRINGS
+console.log(userName);
+console.log(`You live in ${address}`);
+
+//BOOLEANS
+
+let isMale = true;
+let isFemale = false;
+
+//Mixed
+console.log(`are you female? ${userName}: ${isFemale}`);
+
+//manipulation
+document.querySelector(".p1").textContent = userName;
